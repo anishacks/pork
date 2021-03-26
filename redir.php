058ab567370ddc5115a5ef19109176a9569f629d
anishacks <66885893+anishacks@users.noreply.github.com>
2021-03-26 13:59:41 +0530

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,1 +1,1 @@
-<?phpheader("HTTP/1.1 301 Moved Permanently");header("Location: http://localhost:3001");header("Connection: close");?>
+<?phpheader("HTTP/1.1 302 Moved Temporarily");header("Location: http://localhost:3001");header("Connection: close");?>
